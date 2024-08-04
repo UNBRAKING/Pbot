42f24615ab2d898fc22dc30d4c758cf057f8db7e
UNBRAKING <code.pbail@gmail.com>
2024-08-04 13:03:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,4 @@ app.get('/', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-console.log(Server is running on port ${PORT}
-});
+app.listen(PORT, () => {console.log(Server is running on port ${PORT}});
