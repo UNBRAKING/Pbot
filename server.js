db9bba46f97f8835594b27e34e31f1176d7ef46b
UNBRAKING <code.pbail@gmail.com>
2024-08-05 11:53:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+
 // Serve index.html file when root URL is accessed
 app.get('/', (req, res) => {
-    res.send('hello word')
+    res.send('hello world');
 });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>  {console.log(Server is running on port ${PORT})});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
